0f1c4ebb44aed0a08f7ecc59fb890478c385544d
liuzhiliang <imaldway@qq.com>
2022-03-17 15:22:11 +0800

    fix: 首页加载问题

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,16 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useRoute } from 'vue-router'
+import router from './router'
 export default defineComponent({
   setup() {
     console.log('--应用启动--')
+    const route = useRoute()
+    const routes = router.options.routes
+    if (route.path === '' || route.path === '/') {
+      router.push({ path: routes[0].path })
+    }
     return {}
   }
 })
