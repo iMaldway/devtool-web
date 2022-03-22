ac720119cb360ae914531ab84251b1fd80da3628
liuzhiliang <imaldway@qq.com>
2022-03-22 11:24:48 +0800

    feat: 补充404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -131,6 +131,15 @@ const routes = [
       },
     ],
   },
+  {
+    path: "/404",
+    component: () => import("@/views/404.vue"),
+    hidden: true,
+  },
+  {
+    path: "/:catchAll(.*)", // 不识别的path自动匹配404
+    redirect: "/404",
+  },
 ];
 
 export default createRouter({
